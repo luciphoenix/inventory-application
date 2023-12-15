efb4506570556bec6b7b655810b365a3858eb8e3
Alves <luciphoenix.soft@gmail.com>
2023-12-15 16:56:29 +0000

    created models folder

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const { default: mongoose } = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  category_name: { type: String, require: true },
+  category_description: { type: String, require: true },
+});
+
+// virtual properties
+categorySchema.virtual("url").get(function () {
+  return "/category/" + this._id;
+});
+
+module.exports = mongoose.model("Category", categorySchema);
---
+++ models/item.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: { type: String, require: true },
+  description: { type: String, require: true },
+  category: [],
+  price: { type: Number, require: true },
+  number_in_stock: { type: String, require: true },
+});
+
+// virtual properties
+itemSchema.virtual("url").get(function () {
+  return "/item/" + this._id;
+});
+
+module.exports = mongoose.model("Item", itemSchema);
